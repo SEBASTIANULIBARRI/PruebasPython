21778fb33024b6913ca97f2d9e210fa681f54ae7
sebastianulibarri <sebastiannahuelulibarri@gmail.com>
2023-10-07 11:05:42 -0300

    first commit

---
+++ fizzbuzz.py
@@ -0,0 +1,22 @@
+# Escribe un programa que muestre por consola (con un print) los
+# números de 1 a 100 (ambos incluidos y con un salto de línea entre
+# cada impresión), sustituyendo los siguientes:
+# - Múltiplos de 3 por la palabra "fizz".
+# - Múltiplos de 5 por la palabra "buzz".
+# - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+
+
+def replace(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "fizzBuzz"
+    elif num % 5 == 0:
+        return "Buzz"
+    elif num % 3 == 0:
+        return "fizz"
+    else:
+        return num
+
+
+for i in range(1, 101):
+    i = replace(i)
+    print(i)

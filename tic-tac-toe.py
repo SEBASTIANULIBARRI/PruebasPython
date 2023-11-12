4e25d4db4dc7a9b1199e7de1f49fefa4c37fa41c
sebastianulibarri <sebastiannahuelulibarri@gmail.com>
2023-11-11 21:43:41 -0300

    First Commit

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -2,7 +2,8 @@ import colorama
 from colorama import Fore
 from colorama import Style
 valorPorDefecto = "-"
-tablero =[valorPorDefecto,valorPorDefecto,valorPorDefecto,valorPorDefecto,valorPorDefecto,valorPorDefecto,valorPorDefecto,valorPorDefecto,valorPorDefecto]
+JugadasPosibles = ["X","O"]
+
 def printBoard ():
     for i in range(len(tablero)):
         print(tablero[i],end=" ")
@@ -18,7 +19,7 @@ def printBoardFinal (solucion):
         if (i + 1) % 3 == 0:
             print()
 
-def evaluar ():
+def evaluarTablero ():
     result = 0
        # 0
     if (tablero[0] == tablero[1] == tablero[2] and tablero[0]!=valorPorDefecto)  :
@@ -54,6 +55,7 @@ def evaluar ():
     if result == 0: 
         if tablero.count(valorPorDefecto) == 0 : 
             print ("empate")
+            printBoard() 
             return False
         return True
     else: 
@@ -62,19 +64,24 @@ def evaluar ():
 
 def jugada (turno):
     while True:
-        jugada =input("Ingrese un numero: ")
-        if jugada.isdigit():
-            jugada = int(jugada) -1
-            if tablero[jugada ] == valorPorDefecto and jugada <=8 and jugada >= 0:
-                if turno == 1:
-                    tablero[jugada]="X"
-                else : tablero[jugada]="O"
-                break
+    
+        jugadaActual =input(f"Ingrese un numero para {JugadasPosibles[turno]}: ")
+        if jugadaActual.isdigit():
+            jugadaActual = int(jugadaActual) -1
+            if  jugadaActual <=8 and jugadaActual >= 0:
+                if tablero[jugadaActual ] == valorPorDefecto :
+
+                    tablero[jugadaActual]=JugadasPosibles[turno]
+
+                    break
+                else: 
+                    print("jugada invalida La celda ya este ocupada")
+                    printBoard()
             else: 
-                print("jugada invalida vuelve a intentarlo")
+                print("jugada invalida por favor ingrese un numero entre 1 y 9")
                 printBoard()
         else: 
-            print("jugada invalida vuelve a intentarlo")
+            print("jugada invalida por favor ingrese un numero entre 1 y 9")
             printBoard()
 
 def play ():
@@ -82,19 +89,49 @@ def play ():
 
     while numJugadas < 9:  
    
-        turno = 1
-        while turno < 3: 
+        turno = 0
+        while turno < 2: 
             printBoard()
             jugada(turno)
             numJugadas+=1                
             turno+=1
             if numJugadas >= 5: 
-                continuar = evaluar()
-                if not(continuar) :
-
+                continuarJugando = evaluarTablero()
+                if not(continuarJugando) :
                     return
-        
+print(
+f"""Bienvenido a Tic Tac Toe
+>se necesitan dos jugadores para empezar.   
+>El juego consiste en ir dibujando X/O en las diferentes celdas.
+>La forma de ganar es que alguno de los dos pueda lograr que tres de las posibilidades (X/O) queden continuas,
+puede ser de forma horizontal, vertical o diagonal. 
+Ejemplos de cuando gana X:
+   Diagonal  Horizontal  Vertical
+    X O X     X X X       X O -
+    O X O     O O -       X O -
+    X - -     - - -       X - -
 
+>Instrucciones de juego
 
-
-play()
+Para poder jugar se le va a solicitar el numero en donde quiere depositar su jugada. 
+Las posibilidades son:
+    1 2 3
+    4 5 6 
+    7 8 9
+Aclaracion importante, si la celda esta ocupada o se ingresa alguna posicion distinta a las que son 
+validas para jugar se le notificara que la jugada es invalida y se le va a pedir que ingrese nuevamente 
+la posicion.
+""")        
+jugar = input ("presione enter para continuar")
+jugar = True
+while jugar:
+    tablero =[valorPorDefecto,valorPorDefecto,valorPorDefecto,valorPorDefecto,valorPorDefecto,valorPorDefecto,valorPorDefecto,valorPorDefecto,valorPorDefecto]
+    play()
+    continuar = input ("quiere seguir jugando: ingrese n o no para salir, y cualquier otra tecla para continuar jugando: ")
+    print(continuar)
+    continuar = continuar.lower()
+    if continuar.startswith("n"):
+        jugar = False
+        print("Usted ha decidido cerrar el juego. Saludos")
+    
+    
